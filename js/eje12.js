bf767a84b7e3bfc7947f0f194accb6e33f4f7c46
leidy <solarteleidy690@gmail.com>
2026-05-05 03:38:12 +1200

    Primer commit con configuración y código

---
+++ js/eje12.js
@@ -0,0 +1,12 @@
+function fnMaximoVectorFor(array) {
+    if (array.length === 0) return null;
+
+    let max = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+    return max;
+}
+export { fnMaximoVectorFor };
---
+++ js/eje13.js
@@ -0,0 +1,13 @@
+function fnBuscarElementoWhile(data, value) {
+    let index = 0;
+
+    while (index < data.length) {
+        if (data[index] === value) {
+            return index;
+        }
+        index++;
+    }
+
+    return -1;
+}
+export { fnBuscarElementoWhile };
